336dcf44c41f45381cec77e5aae3d150eeda7593
Jose Soto <korxaq@gmail.com>
2017-05-17 18:29:50 -0600

    Angular 2 Training material 1

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './home.component.html'
+})
+
+export class HomeComponent implements OnInit{
+    variable1: any;
+    variable2: string;
+
+    constructor() {}
+
+    ngOnInit(){
+
+    }
+
+}
